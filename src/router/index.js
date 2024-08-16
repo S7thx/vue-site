a57d70cb3b8e93f48f0c82fc017b43833180cb19
S7thx <136721499+S7thx@users.noreply.github.com>
2024-08-16 11:05:03 -0300

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import VideoComponent from '@/components/VideoComponent.vue'
-import ImagemComponent from '@/components/ImagemComponent.vue'
-import TabelaComponent from '@/components/TabelaComponent.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,24 +6,23 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('../views/HomeView.vue')
     },
     {
      path: '/videos',
      name: 'videos',
-     component: VideoComponent,
+     component: () => import('../components/VideoComponent.vue')
     },
     {
      path: '/imagens',
      name: 'imagens',
-     component: ImagemComponent,
+     component: () => import('../components/ImagemComponent.vue')
     },
     {
      path: '/tabela',
      name: 'tabela',
-     component: TabelaComponent,
+     component: () => import('../components/TabelaComponent.vue')
     },
   ]
 })
-
 export default router
