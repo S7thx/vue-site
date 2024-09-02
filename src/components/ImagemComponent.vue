cc6ae7a0d132181625dc93ccbfe6aba6b4e85d22
S7thx <136721499+S7thx@users.noreply.github.com>
2024-09-02 10:02:14 -0300

    Add files via upload

--- src/components/ImagemComponent.vue
+++ src/components/ImagemComponent.vue
@@ -1,6 +1,7 @@
 <template>
      <div id="png" style="width: 33%;">
         <img width="100%" src="https://th.bing.com/th?id=OIP.NLtWGtzFhsFQf83Wmam0kwHaKh&w=209&h=298&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2">
+        <img src="/src/assets/imagem/img3.jpg">
     </div>
     <div id="img" style="width: 33%; display: flex;">
         <img width="50%" src="https://th.bing.com/th?id=OIP.hN87iLXD_xAtuM4hjvHFvAHaG4&w=259&h=241&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" >
